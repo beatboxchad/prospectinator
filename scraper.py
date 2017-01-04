997eafc09a1ce03a9a7bbaca10b42f66d49e6b3e
Chad Cassady <chad.cassady@gmail.com>
2017-01-03 16:33:49 -0800

    Added JSON cache to save on network and speed development

--- scraper.py
+++ scraper.py
@@ -6,15 +6,24 @@ import os
 from pprint import pprint
 from blindelephant.Fingerprinters import WebAppFingerprinter
 
+MAX_WP_VERSION = "4.7" #FIXME stop hardcoding
+
 # a piece of the dotfile search bit -- TODO make this mroe dynamic.
 # xdg-settings, whatever on mac. Also implement CL args
-CONFIG_PATH    = os.path.expanduser("~/.config/")
+CONFIG_PATH    = os.path.expanduser("~/.config/prospectinator/")
+if not os.path.exists(CONFIG_PATH):
+        os.makedirs(CONFIG_PATH)
 
-MAX_WP_VERSION = "4.7" #FIXME stop hardcoding
-places         = []
+CACHE = open(CONFIG_PATH + "cache.json", 'r')
 
-with open(CONFIG_PATH + 'prospectinator.json') as defaults_file:    
+with open(CONFIG_PATH + 'prospectinator.json', 'r') as defaults_file:    
     defaults = json.load(defaults_file)
+
+
+try: 
+    places = json.load(CACHE)
+except:
+    places = {}
 
 gmaps_apikey = defaults["gmaps_apikey"]
 location     = defaults["location"]
@@ -25,13 +34,22 @@ query_url = base_url % (location, gmaps_apikey)
 response  = json.loads(urllib2.urlopen(query_url).read())
 
 #now we get the details of each place.
-ids = [result["place_id"] for result in query_response["results"]]
+ids        = [result["place_id"] for result in response["results"]]
+place_url  = "https://maps.googleapis.com/maps/api/place/details/json?placeid=%s&key=%s"
 
-base_url1 = "https://maps.googleapis.com/maps/api/place/details/json?placeid=%s&key=%s"
 for place_id in ids:
-    my_query_url = base_url1 % (place_id, gmaps_apikey)
-    places.append(json.loads(urllib2.urlopen(my_query_url).read()))
+    # FIXME gross name
+    my_query_url = place_url % (place_id, gmaps_apikey)
+    # FIXME better interrogation? Actually for this stage this is fine. We can
+    # inspect the data for fingerprinted later
+    if not place_id in places:
+        places[place_id] = json.loads(urllib2.urlopen(my_query_url).read())
 
+CACHE.close()
+
+NEW_CACHE = open(CONFIG_PATH + "cache.json", 'w')
+NEW_CACHE.write(json.dumps(places, indent=2, sort_keys=True))
+NEW_CACHE.close()
 
 # now we want to filter places based on whether the fingerprinter found an outdated WP
 
