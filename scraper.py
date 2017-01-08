9e8f455f698e25ece6e14f7d15b6d5cf6fe4007f
Chad Cassady <chad.cassady@gmail.com>
2017-01-08 13:34:49 -0800

    Switching to mongodb for cache, moved to new fingerprinter. all in-progress

--- scraper.py
+++ scraper.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 import json
-import urllib2
+import urllib.request
 import os
+import re
 
 from pprint import pprint
-from blindelephant.Fingerprinters import WebAppFingerprinter
-
-MAX_WP_VERSION = "4.7" #FIXME stop hardcoding
+from wig.wig import wig
+from wig.classes.output import OutputJSON
+from pymongo import MongoClient
+from multiprocessing import Pool
+pool = Pool()
 
 # a piece of the dotfile search bit -- TODO make this mroe dynamic.
 # xdg-settings, whatever on mac. Also implement CL args
@@ -19,53 +22,56 @@ CACHE = open(CONFIG_PATH + "cache.json", 'r')
 with open(CONFIG_PATH + 'prospectinator.json', 'r') as defaults_file:    
     defaults = json.load(defaults_file)
 
-
 try: 
     places = json.load(CACHE)
 except:
     places = {}
 
-gmaps_apikey = defaults["gmaps_apikey"]
-location     = defaults["location"]
+gmaps_apikey  =  defaults["gmaps_apikey"]
+location      =  defaults["location"]
+dbhost        =  defaults["dbhost"]
+dbport        =  defaults["dbport"]
+
+client  =  MongoClient(dbhost, dbport)
+DB      =  client.prospectinator
 
 # grab the list of places
 base_url  = "https://maps.googleapis.com/maps/api/place/radarsearch/json?location=%s&keyword=*&&radius=5000&key=%s"
-query_url = base_url % (location, gmaps_apikey)
-response  = json.loads(urllib2.urlopen(query_url).read())
+query_url = urllib.request.Request(base_url % (location, gmaps_apikey))
+response  = json.loads(urllib.request.urlopen(query_url).read().decode('utf-8'))
 
-#now we get the details of each place.
+# now we get the details of each place.
 ids        = [result["place_id"] for result in response["results"]]
 place_url  = "https://maps.googleapis.com/maps/api/place/details/json?placeid=%s&key=%s"
-
-for place_id in ids:
+    
+def run_fingerprint(place_id):
     # FIXME gross name
-    my_query_url = place_url % (place_id, gmaps_apikey)
+    print(place_id)
+    my_query_url = urllib.request.Request(place_url % (place_id, gmaps_apikey))
     # FIXME better interrogation? Actually for this stage this is fine. We can
     # inspect the data for fingerprinted later
     if not place_id in places:
-        places[place_id] = json.loads(urllib2.urlopen(my_query_url).read())
+        places[place_id] = json.loads(urllib.request.urlopen(my_query_url).read().decode('utf-8'))
 
+    if 'website' in places[place_id]['result']:
+        if 'fingerprint' not in places[place_id]:
+            places[place_id]['fingerprint'] = []
+            w = wig(url = places[place_id]['result']['website'])
+            w.run()
+            results = OutputJSON(w.options, w.results)
+            try:
+                results.add_results()
+            except KeyError:
+                pass
+
+            places[place_id]['fingerprint'].append(results.json_data)
+    return
+
+pool.map(run_fingerprint, ids)
+
+
+#cleanup and write
 CACHE.close()
-
 NEW_CACHE = open(CONFIG_PATH + "cache.json", 'w')
 NEW_CACHE.write(json.dumps(places, indent=2, sort_keys=True))
 NEW_CACHE.close()
-
-# now we want to filter places based on whether the fingerprinter found an outdated WP
-
-# Some fingerprinter subs are in order, which I can use as a filter fn.
-
-# I have decided to save the data as JSON. Indeed, perhaps I'll build my own
-# data structure and write it to disk between invocations. Yes!! Cache the
-# place details. Like this:
-
-#{ 
-#        'some_place_id' : { 'all the data' : 'etc' },
-#        'other_place_id' : { 'all the data' : 'etc' },
-#        }
-
-# Write that to a file, and don't ask the places API for information on places
-# we already have. Brilliant. Write that next so that dev will be faster. Read
-# it all at each invocation
-
-# cache the fingerprint results too.
